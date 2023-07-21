7750a88e182954b488bd2ccb25eb62551a808fad
nedious <ned.imhoff@gmail.com>
2023-07-20 21:42:30 -0600

    update to UTC

--- src/main/java/Helper/JDBC.java
+++ src/main/java/Helper/JDBC.java
@@ -15,7 +15,7 @@ public abstract class JDBC {
     private static final String vendor = ":mysql:";
     private static final String location = "//localhost/";
     private static final String databaseName = "client_schedule";
-    private static final String jdbcUrl = protocol + vendor + location + databaseName + "?connectionTimeZone = UTC"; // LOCAL  // points to database
+    private static final String jdbcUrl = protocol + vendor + location + databaseName + "?connectionTimeZone = UTC"; // LOCAL   // points to database
     private static final String driver = "com.mysql.cj.jdbc.Driver"; // Driver reference
     private static final String userName = "sqlUser";                // MySQL database Username
     private static String password = "Passw0rd!";                    // MySQL database Password
